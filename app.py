94e2b1d532ba4184eba19ee892b2dfd1d23d146d
gilyeon00 <12venus08@naver.com>
2022-09-02 11:00:54 +0900

    Feat : add gunicorn, router

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 app.config.update(DEBUG=True)
 
+@app.route('/hello')
+def welcome():
+    return 'Hello world'
+
+
 if __name__ == "__main__":
     app.run(port=5123, debug=True)

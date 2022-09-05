db1e3a2f694b377c5cfe77b826ecbbb3e3b4f6f2
gilyeon00 <12venus08@naver.com>
2022-09-06 02:13:56 +0900

    Feat : #5 Add image table

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import request
 
 from __init__ import create_app
 from entity import database
-from entity.model import User
+from entity.model import User, Image
 
 
 app = create_app()
@@ -31,7 +31,7 @@ def fetch():
 
 ## Create User
 @app.route('/create-user', methods=['POST'])
-def add():
+def create_user():
     data = request.get_json()
     email = data['email']
     name = data['name']
@@ -40,3 +40,14 @@ def add():
     database.add_instance(User, email=email, name=name, password=password)
     
     return json.dumps("Added"), 200
+
+
+## Uploade Image 
+@app.route('/upload-image', methods=['POST'])
+def upload_image():
+    data = request.get_json()
+    user_id = data['userid']
+    image_url = data['imageurl']
+    database.add_instance(Image, user_id = user_id, image_url = image_url)
+    
+    return json.dumps("Image Added"), 200
--- entity/model.py
+++ entity/model.py
@@ -1,5 +1,6 @@
 import flask_sqlalchemy
 
+
 db = flask_sqlalchemy.SQLAlchemy()
 
 class User(db.Model):
@@ -9,6 +10,16 @@ class User(db.Model):
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
     is_active = db.Column(db.Boolean, default=True)
+    create_at = db.Column(db.TIMESTAMP, server_default = db.func.now() )
+    upload_at = db.Column(db.TIMESTAMP, server_default = db.func.now() )
+
+
+class Image(db.Model):
+    __tablename__ = 'image'
+    image_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+
+    image_url = db.Column(db.String(150))
     create_at = db.Column(db.TIMESTAMP, server_default = db.func.now() )
     upload_at = db.Column(db.TIMESTAMP, server_default = db.func.now() )
 

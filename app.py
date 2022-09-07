ee678a5c57dab3c6b487c1fd91732a36bc2cf0cd
gilyeon00 <12venus08@naver.com>
2022-09-07 18:38:09 +0900

    Feat : #8 aws 1차 세팅, front 확인 필요(upload)

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ from entity import database
 from entity.model import User, Image
 from entity.model import db
 
+from crypt import methods
+from urllib import request
+import datetime as dt
+from s3bucket.s3_connect import s3
+from s3bucket.s3_upload import s3_put_object
+
+
+app = Flask(__name__)
+app.config.update(DEBUG=True)
+
 app = create_app()
 
 @app.route('/')
@@ -70,3 +80,27 @@ def fetch_images():
         all_image.append(new_image)
 
     return json.dumps(all_image), 200
+
+
+@app.route('/s3-image-upload-test', methods=['POST'])
+def s3upload_test():
+    # html에서 가져온 이미지 
+    file = request.file['file_give']
+
+    # 파일 이름 지정
+    filename = file.filename.split('.')[0]
+    ext = file.filename.split('.')[-1]
+    img_name = dt.datetime.now().strtime(f"{filename}--%Y-%m-%d-%H-%M-%S.{ext}")
+
+    # 현재 로그인 사용자 정보
+    
+    # s3버킷에 업로드
+    s3_put_object(s3, 'ladder-s3-bucket', file, img_name)
+
+    # postgres image table에 업로드
+
+
+
+
+if __name__ == "__main__":
+    app.run(port=5123, debug=True)

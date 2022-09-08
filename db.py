296fd34bc4867dc1e2a342db9a36c4514fc0cced
bun0531 <112270652+bun0531@users.noreply.github.com>
2022-09-08 13:31:16 +0900

    Create db.py
    Signed-off-by: bun0531 <112270652+bun0531@users.noreply.github.com>

---
+++ db.py
@@ -0,0 +1,28 @@
+import pymysql
+
+class Database():
+    def__init__(self):
+    self.db=pymysql.connect(host='db.host.com',
+                                  user='user_name',
+                                  password='user_password',
+                                  db='db_name',
+                                  charset='utf8')
+        self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
+
+    def execute(self, query, args={}):
+        self.cursor.execute(query, args)
+
+    def executeOne(self, query, args={}):
+        self.cursor.execute(query, args)
+        row = self.cursor.fetchone()
+        return row
+
+    def executeAll(self, query, args={}):
+        self.cursor.execute(query, args)
+        row = self.cursor.fetchall()
+        return row
+
+    def commit(self):
+        self.db.commit()
+
+

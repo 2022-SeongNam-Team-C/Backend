74300e2aaf9c7f951bf25979d2ca4b5c5d92a28b
bun0531 <112270652+bun0531@users.noreply.github.com>
2022-09-14 13:01:46 +0900

    Update app.py
    Signed-off-by: bun0531 <112270652+bun0531@users.noreply.github.com>

--- app.py
+++ app.py
@@ -18,15 +18,13 @@ app = create_app()
 
 
 app.config['JWT_SECRET_KEY'] = 'Team C'
-# 1800 JWT 만료 시간 설정 부분임 (단위 : 초)
+
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 30
-# 604800
-app.config['JWT_REFRESH_TOKEN_EXPIRES'] = 50
+
+app.config['JWT_REFRESH_TOKEN_EXPIRES'] = 604800
 jwt = JWTManager(app)
 
 
-# app.config['SECRET_KEY'] = 'Team C'
-# app.config['BCRYPT_LEVEL'] = 10
 bcrypt = Bcrypt(app)
 
 
@@ -46,23 +44,16 @@ def login():
     if not password:
         return jsonify({"msg": "Missing password parameter"}), 400
 
-    # 데이터베이스에서 비밀번호 가져오기
-    # 그리고 입력한 비밀번호와 비교
-    # if not bcrypt.check_password_hash(password, '12341234').decode('utf-8'):
-    #    return jsonify(msg="비밀번호를 확인해주세요."), 403
+  
     user = User.query.filter_by(email=email).all()[0]
     if not User.check_password(user, password):
         return jsonify(msg="비밀번호를 확인해주세요."), 403 
 
 
-    # 액세스토큰과 리프레시토큰 생성하는 코드
     access_token = create_access_token(identity=email)
     refresh_token = create_refresh_token(identity=email)
     response = jsonify(access_token=access_token, refresh_token=refresh_token)
 
-    # set_refresh_token_db(email=email, refresh_token=refresh_token)
-
-    # 토큰 쿠키에 저장
     set_access_cookies(response=response, encoded_access_token=access_token)
     set_refresh_cookies(response=response, encoded_refresh_token=refresh_token)
 
@@ -95,20 +86,8 @@ def register():
 
           
 
-    # 회원가입 암호화 코드 ( encrypted_password 를 디비에 저장 )
-    # encrypted_password = bcrypt.generate_password_hash(password=password.encode('utf-8'), rounds=10).decode()
-    
-  
-
-    # 데이터베이스에 저장하는 코드 들어갈 부분
     database.add_instance(User, name=name, email=email, password=password)
 
-
-    
-    #디비에 저장된 비밀번호입니다.
-    #db_pwd="$2b$10$oheJjGmtFzuide30FANYh.nodyigWs.5hbAZSswGPYJ6MzUDCMcHy"
-    #if not bcrypt.check_password_hash(pw_hash=db_pwd, password=password):
-    #    return jsonify(msg="비밀번호를 확인해주세요."),403
         
     return jsonify(msg="success"), 201
 

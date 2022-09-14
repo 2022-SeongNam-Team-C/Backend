20eff9533e54716e9daba0176adc890eefbb0f1b
gilyeon00 <12venus08@naver.com>
2022-09-14 18:18:46 +0900

    Fix : #8 db colums type

--- entity/model.py
+++ entity/model.py
@@ -7,8 +7,8 @@ db = flask_sqlalchemy.SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'user'
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(150), unique=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     is_active = db.Column(db.Boolean, default=True)
     create_at = db.Column(db.TIMESTAMP, server_default = db.func.now() )

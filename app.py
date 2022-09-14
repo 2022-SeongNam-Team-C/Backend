e48a4defec57282b7d509884439fd54f0b68d57a
gilyeon00 <12venus08@naver.com>
2022-09-15 03:27:12 +0900

    Chore : #8 모듈 분리 작업중

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ from api.s3_api import bp as s3_module
 from crypt import methods
 from datetime import datetime as dt
 
-from werkzeug.utils import secure_filename
-from s3bucket.s3_connect import s3
-from s3bucket.s3_upload import s3_put_object
-
 
 app = Flask(__name__)
 app.config.update(DEBUG=True)
@@ -88,32 +84,6 @@ def fetch_images():
         all_image.append(new_image)
 
     return json.dumps(all_image), 200
-
-
-# @app.route('/s3-image-upload-test', methods=['POST'])
-# def s3upload_test():
-#     # html에서 가져온 이미지 
-#     file = request.files['file']
-#     file.save(secure_filename(file.filename))
-
-#     # 파일 이름 지정
-#     filename = file.filename.split('.')[0]
-#     image_type = file.filename.split('.')[-1]
-#     image_name = dt.now().strftime(f"{filename}--%Y-%m-%d-%H-%M-%S.{image_type}")
-    
-#     # s3버킷에 업로드
-#     s3_put_object(s3, 'ladder-s3-bucket', file, image_name)
-
-#     # 현재 로그인 사용자 정보
-#     user_id = 1
-
-#     # postgres image table에 업로드
-#     image_url = "s3://ladder-s3-bucket/images/"+image_name
-#     image_url = image_url.replace(" ","/")
-#     database.add_instance(Image, user_id = user_id, image_url = image_url)
-
-#     return "성공적으로 사진이 S3에 저장되었습니다."
-
 
 
 

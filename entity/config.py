f5dd05617e316c366e791028e3e99198d5578909
gilyeon00 <12venus08@naver.com>
2022-09-21 00:02:45 +0900

    Feat : #2 Edit DB url

--- entity/config.py
+++ entity/config.py
@@ -8,8 +8,7 @@ database = os.getenv('PG_DB')
 port = os.getenv('PG_PORT')
 
 
-#.env 파일을 잘 못 불러와, 우선 명시적으로 해놨습니다.
-DATABASE_CONNECTION_URI = f'postgresql+psycopg2://postgres:2217@postgres:5432/postgres'
+DATABASE_CONNECTION_URI = f'postgresql+psycopg2://darling:0829@ladderdb:5432/ladderdb'
 jwt_redis = redis.StrictRedis(host='redis', port=6379, db=0, decode_responses=True)
 
 

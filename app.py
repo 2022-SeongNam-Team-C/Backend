3a866f12bbb78c4958df08470fd382cd4e010eb2
hee <jkgt0521@naver.com>
2022-09-29 01:55:18 +0900

    Feat: #14 Prometheus and Grafana connection

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import imp
 from os import access
-import secrets
 from flask import jsonify, request
 from flask_jwt_extended import ( JWTManager, jwt_required, create_access_token, create_refresh_token, get_jwt_identity)
 from config.auth import (deauthenticate_user,
@@ -8,6 +7,7 @@ from config.auth import (deauthenticate_user,
                   auth_required, AuthenticationError)
 from flask_bcrypt import Bcrypt
 import redis
+from flask import Response
 from __init__ import create_app
 from entity import database
 from entity.model import User
@@ -23,9 +23,10 @@ from api.history_api import History
 from crypt import methods
 from flask_cors import CORS
 import jwt as pyjwt
-from module.check_token import check_access_token
-# from jwt.exception import ExpiredSignatureError
+from prometheus_client import generate_latest, REGISTRY, Counter, Gauge, Histogram
 
+
+# flask App
 app = create_app()
 CORS(app)
 app.config.update(DEBUG=True)
@@ -33,27 +34,34 @@ app.register_blueprint(email_module, url_prefix = '/api/v1')
 app.register_blueprint(s3_module, url_prefix = '/api/v1')
 app.register_blueprint(history_module, url_prefix = '/api/v1')
 
-
+# Swagger
 api = Api(app, version=1.0, title="ladder api", description='ladder api docs', doc='/api-docs')  # Flask 객체에 Api 객체 등록
 ladder_api = api.namespace('api/v1', description='ladder api docs')
-
 api.add_namespace(Email, '')
 # api.add_namespace(s3, '')
 api.add_namespace(History, '')
 
+# login token
 secrets_key = 'Ladder_teamc'
-
 app.config['JWT_SECRET_KEY'] = secrets_key  # JWT 시크릿 키
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 60 * 60
 app.config['JWT_REFRESH_TOKEN_EXPIRES'] = 60 * 60 * 24 * 14
 app.config['JWT_TOKEN_LOCATION'] = ['json']   # jwt 토큰을 점검할 때 확인할 위치
 jwt = JWTManager(app)
-
 jwt_redis = redis.StrictRedis(host='redis', port=6379, decode_responses=True)
 bcrypt = Bcrypt(app)
 
+
+# prometheus
+REQUESTS = Counter('http_requests_total', 'Total HTTP Requests (count)', ['method', 'endpoint', 'status_code'])
+IN_PROGRESS = Gauge('http_requests_inprogress', 'Number of in progress HTTP requests')
+TIMINGS = Histogram('http_request_duration_seconds', 'HTTP request latency (seconds)')
+
+
 @ladder_api.route('/auth/signin')    # login api
 class Signin(Resource):
+    @TIMINGS.time()
+    @IN_PROGRESS.track_inprogress()
     def post(self):
         if not request.is_json:
             return {"error": "Missing JSON data in request."}, 400
@@ -62,16 +70,20 @@ class Signin(Resource):
         password = request.json.get('password')
         
         if not email:
+            REQUESTS.labels(method='POST', endpoint="/auth/signin", status_code=400).inc()  
             return {"error": "Missing email parameter."}, 400
         if not password:
+            REQUESTS.labels(method='POST', endpoint="/auth/signin", status_code=400).inc()  
             return {"error": "Missing password parameter."}, 400
     
         user = User.query.filter_by(email=email).all()
         
         if len(user) == 0:
+            REQUESTS.labels(method='POST', endpoint="/auth/signin", status_code=404).inc()  
             return {"error": "We can't find this user."}, 404
 
         if not User.check_password(user[0], password):
+            REQUESTS.labels(method='POST', endpoint="/auth/signin", status_code=403).inc()  
             return {"error": "Please check your email and password."}, 403
 
         user_refresh_key = email + '_refresh'
@@ -92,15 +104,19 @@ class Signin(Resource):
             "refresh_token": refresh_token,
             "name": user_name
         }
+        REQUESTS.labels(method='POST', endpoint="/auth/signin", status_code=200).inc()  
         return response_dict, 200 
 
 @ladder_api.route('/auth/signout')   # logout api
 class Signout(Resource):
-    def post(self):
+    @TIMINGS.time()
+    @IN_PROGRESS.track_inprogress()
+    def post(self): 
         header_request = request.headers
         bearer = header_request.get('Authorization')
 
         if not bearer:
+            REQUESTS.labels(method='POST', endpoint="/auth/signout", status_code=401).inc() 
             return {"error": "You don't have access authentication."}, 401
 
         access_token = bearer.split()[1]
@@ -117,15 +133,20 @@ class Signout(Resource):
                 "user": user
             }
 
+            REQUESTS.labels(method='POST', endpoint="/auth/signout", status_code=200).inc() 
             return response_dict, 200
         except pyjwt.ExpiredSignatureError:
+            REQUESTS.labels(method='POST', endpoint="/auth/signout", status_code=401).inc() 
             return {"error": "This Token is expired."}, 401
 
 
 @ladder_api.route('/auth/signup')   # register users api
 class Signup(Resource):
-    def post(self):
+    @TIMINGS.time()
+    @IN_PROGRESS.track_inprogress()
+    def post(self):  
         if not request.is_json:
+            REQUESTS.labels(method='POST', endpoint="/auth/signup", status_code=400).inc()
             return {"error": "Missing JSON in request."}, 400
 
         email = request.json.get('email')
@@ -133,15 +154,19 @@ class Signup(Resource):
         name = request.json.get('name')
 
         if not email:
+            REQUESTS.labels(method='POST', endpoint="/auth/signup", status_code=400).inc()
             return {"error": "Missing email parameter."}, 400
         if not password:
+            REQUESTS.labels(method='POST', endpoint="/auth/signup", status_code=400).inc()
             return {"error": "Missing password parameter."}, 400
         if not name:
+            REQUESTS.labels(method='POST', endpoint="/auth/signup", status_code=400).inc()
             return {"error": "Missing name parameter."}, 400
 
         # 이메일 중복검사
         user = User.query.filter_by(email=email).all()
         if len(user) != 0:
+            REQUESTS.labels(method='POST', endpoint="/auth/signup", status_code=409).inc()
             return {"error": "This email is already registered."}, 409
             # 409 conflict error, 리소스 충돌을 의미하는 상태코드 전송
             # 리소스의 현재 상태와 충돌해서 해당 요청을 처리할 수 없어 클라이언트가 충돌을 수정해서 다시 요청을 보내야할 때 사용
@@ -154,15 +179,19 @@ class Signup(Resource):
             "name": User.query.filter(User.email == email).first().name
         }
 
-        return user_dict
+        REQUESTS.labels(method='POST', endpoint="/auth/signup", status_code=200).inc()
+        return user_dict, 200
 
 @ladder_api.route('/auth/refresh')  # refresh access token api
 class Resignin(Resource):
+    @TIMINGS.time()
+    @IN_PROGRESS.track_inprogress()
     def get(self):
         header_request = request.headers
         bearer = header_request.get('Authorization')
 
         if not bearer:
+            REQUESTS.labels(method='GET', endpoint="/auth/refresh", status_code=401).inc()  
             return {"error": "You don't have access authentication."}, 401
 
         refresh_token = bearer.split()[1]
@@ -172,17 +201,27 @@ class Resignin(Resource):
 
         is_refresh = jwt_redis.get(user_refresh_key)
         if not is_refresh:
+            REQUESTS.labels(method='GET', endpoint="/auth/refresh", status_code=401).inc()  
             return {"msg": "This is a invalid user."}, 401
 
         if refresh_token != is_refresh:
+            REQUESTS.labels(method='GET', endpoint="/auth/refresh", status_code=500).inc()  
             return {"msg": "server error"}, 500
 
         access_token = create_access_token(identity=user)
         response_dict = {
             "access_token": access_token
         }
+        REQUESTS.labels(method='GET', endpoint="/auth/refresh", status_code=200).inc()  
         return response_dict, 200
 
+@app.route('/metrics')
+@IN_PROGRESS.track_inprogress()
+@TIMINGS.time()
+def metrics():
+    REQUESTS.labels(method='GET', endpoint="/metrics", status_code=200).inc()
+    return generate_latest(REGISTRY)
+    
 
 if __name__ == '__main__':
     app.run(debug=True, port=5123)
